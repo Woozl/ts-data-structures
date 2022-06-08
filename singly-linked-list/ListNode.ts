dbfad29e16e410a08e21c4367a9117f8639da27f
David Glymph <Woozl@users.noreply.github.com>
2022-06-08 18:18:11 -0400

    documented ListNode

--- singly-linked-list/ListNode.ts
+++ singly-linked-list/ListNode.ts
@@ -1,6 +1,17 @@
+/**
+ * Defines a node for a list linked in one direction.
+ * Includes a generic value and pointer to the next node, which
+ * can be null to indicate the end of the list.
+ */
 export default class ListNode<T> {
     val: T;
     next: ListNode<T> | null;
+
+    /**
+     * 
+     * @param val generic value stored in this node
+     * @param next optional argument specifyin the next `ListNode`. If not specified, `this.next` equals `null`.
+     */
     constructor(val: T, next?: ListNode<T>) {
         this.val = val;
         this.next = next ? next : null;
