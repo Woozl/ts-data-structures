d91f1e1d4bb8a0555f8d0a927b28d34921e5b7b6
David Glymph <Woozl@users.noreply.github.com>
2022-06-12 14:45:51 -0400

    hash table

---
+++ data-structures/hash-table/hash.ts
@@ -0,0 +1,15 @@
+const P = 31;
+const M = 4987793;
+
+/**
+ * Polynomial rolling hash function for lowercase string keys
+ * @link https://www.geeksforgeeks.org/string-hashing-using-polynomial-rolling-hash-function/
+ * @param s 
+ * @param p 
+ * @param m 
+ * @returns hash value between 0 and (m-1) inclusive.
+ */
+export default function polynomialRollingHash(s: string, p=P, m=M): number {
+
+    return 0;
+}
