260542b9e15ccda1fd3d03e027919da128d3f2b0
David Glymph <Woozl@users.noreply.github.com>
2022-07-05 17:20:25 -0400

    implementation of Trie

--- data-structures/trie/Trie.ts
+++ data-structures/trie/Trie.ts
@@ -16,15 +16,47 @@ export default class Trie {
      * @param string key to be stored
      */
     insert(string: string) {
+        let currentNode = this.root;
 
+        for(const c of string) {
+            if(currentNode.children.has(c)) {
+                currentNode = currentNode.children.get(c)!;
+            }
+            else {
+                currentNode.children.set(c, new TrieNode());
+                currentNode = currentNode.children.get(c)!;
+            }
+        }
+
+        currentNode.isEndOfWord = true;
     }
 
     /**
-     * Deletes a string key
+     * Deletes a string key if it is in the Trie
      * @param string key to be deleted
      */
     delete(string: string) {
+        const recurse = (s = string, node = this.root, level = 0): boolean => {
+            if(level === s.length) {
+                if(node.children.size > 0) {
+                    node.isEndOfWord = false;
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+            
+            const childDeleted = recurse(s, node.children.get(s[level])!, level + 1);
 
+            if(childDeleted) {
+                node.children.delete(s[level]);
+            }
+
+            return false;
+        };
+
+        recurse();
     }
 
     /**
@@ -33,8 +65,15 @@ export default class Trie {
      * @returns true if the word is in the Trie
      */
     hasWord(string: string): boolean {
+        let currentNode = this.root;
+        
+        for(const c of string) {
+            if(!currentNode.children.has(c)) return false;
 
-        return false;
+            currentNode = currentNode.children.get(c)!;
+        }
+
+        return currentNode.isEndOfWord;
     }
 
     /**
@@ -43,7 +82,14 @@ export default class Trie {
      * @returns true if the word is in the Trie
      */
     hasPrefix(string: string): boolean {
+        let currentNode = this.root;
 
-        return false;
+        for(const c of string) {
+            if(!currentNode.children.has(c)) return false;
+
+            currentNode = currentNode.children.get(c)!;
+        }
+
+        return true;
     }
 }
