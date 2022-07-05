e65eac8e3cf3af999baf9f08c7d65404b07a3f76
David Glymph <Woozl@users.noreply.github.com>
2022-07-05 17:22:42 -0400

    TrieNode documentation

--- data-structures/trie/TrieNode.ts
+++ data-structures/trie/TrieNode.ts
@@ -1,3 +1,9 @@
+/**
+ * Defines a trie node with a `children` variable mapping
+ * string characters to children `TrieNodes` and an 
+ * `isEndOfWord` boolean to represent the end of an inserted
+ * string.
+ */
 export default class TrieNode {
     children: Map<string, TrieNode>;
     isEndOfWord: boolean;
