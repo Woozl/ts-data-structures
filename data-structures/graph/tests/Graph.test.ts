c4e2a2b91c889c2644ea1545a8a824c16d615d23
David Glymph <Woozl@users.noreply.github.com>
2022-07-06 11:23:43 -0400

    graph test skeletons

---
+++ data-structures/graph/tests/Graph.test.ts
@@ -0,0 +1,36 @@
+import Graph from '../Graph';
+
+describe('Graph', () => {
+    it('should create a graph', () => {
+        const g = new Graph();
+        expect(g).toBeInstanceOf(Graph);
+    });
+
+    describe('addVertex', () => {
+        
+    });
+
+    describe('addEdge', () => {
+        
+    });
+
+    describe('deleteVertex', () => {
+
+    });
+
+    describe('deleteEdge', () => {
+        
+    });
+
+    describe('getWeight', () => {
+
+    });
+
+    describe('getNeighbors', () => {
+        
+    });
+
+    describe('print', () => {
+        
+    });
+});
---
+++ data-structures/graph/tests/UndirectedGraph.test.ts
@@ -0,0 +1,16 @@
+import UndirectedGraph from '../UndirectedGraph';
+
+describe('UndirectedGraph', () => {
+    it('should create an undirected graph', () => {
+        const g = new UndirectedGraph<number>();
+        expect(g).toBeInstanceOf(UndirectedGraph);
+    });
+
+    describe('addEdge', () => {
+        
+    });
+
+    describe('deleteEdge', () => {
+        
+    });
+});
