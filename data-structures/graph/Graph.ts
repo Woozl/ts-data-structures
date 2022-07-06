dcf0a0a46a5ed2780c46dc967e36dd2b7f7db284
David Glymph <Woozl@users.noreply.github.com>
2022-07-06 09:59:34 -0400

    simplified graph structure

--- data-structures/graph/Graph.ts
+++ data-structures/graph/Graph.ts
@@ -1,23 +1,99 @@
-import GraphVertex from "./GraphVertex";
-
 export default class Graph<T> {
+    private adjacencyTable: Map<T, Map<T, number> | null>;
+
     constructor() {
-
+        this.adjacencyTable = new Map<T, Map<T, number> | null>();
     }
 
-    addVertex(vertex: GraphVertex<T>) {
+    addVertex(v: T): boolean {
+        if(this.adjacencyTable.has(v))
+            return false;
 
+        this.adjacencyTable.set(v, null);
+        return true;
     }
 
-    addEdge(u: GraphVertex<T>, v: GraphVertex<T>, weight: number) {
+    addEdge(u: T, v: T, weight: number = 1): boolean {
+        if(!this.adjacencyTable.has(u) || !this.adjacencyTable.has(v))
+            return false;
 
+        // if the `u` node in the adjacency map already has a corresponding
+        // destination map, add `v` and `weight` to it, otherwise, create a new
+        // map, then add `v` and `weight` to it.
+        if(this.adjacencyTable.get(u) !== null) {
+            this.adjacencyTable.get(u)!.set(v, weight);
+        }
+        else {
+            const destinationTable = new Map<T, number>();
+            this.adjacencyTable.set(u, destinationTable);
+            destinationTable.set(v, weight);
+        }
+
+        return true;
     }
 
-    deleteVertex(vertex: GraphVertex<T>): T | null {
-        return null;
+    deleteVertex(v: T): boolean {
+        if(!this.adjacencyTable.has(v)) return false;
+
+        this.adjacencyTable.delete(v);
+
+        this.adjacencyTable.forEach(destinationTable => 
+            destinationTable?.delete(v)
+        );
+
+        return true;
     }
 
-    deleteEdge(u: GraphVertex<T>, v: GraphVertex<T>): number {
-        return 0;
+    deleteEdge(u: T, v: T): boolean {
+        if(!this.adjacencyTable.has(u) || !this.adjacencyTable.has(v))
+            return false;
+
+        let hadV = this.adjacencyTable.get(u)!.delete(v);
+
+        if(this.adjacencyTable.get(u)!.size === 0)
+            this.adjacencyTable.set(u, null);
+
+        return hadV;
+    }
+
+    getWeight(u: T, v: T): number | null {
+        if(!this.adjacencyTable.has(u) || !this.adjacencyTable.has(v))
+            return null;
+
+        return this.adjacencyTable.get(u)?.get(v) ?? null;
+    }
+
+    print(printWeight = false) {
+        this.adjacencyTable.forEach((destTable, sourceNode) => {
+            let str = `${sourceNode} → `;
+
+            if(destTable) {
+                str += '[';
+                let destTableSize = destTable.size;
+                destTable.forEach((weight, destNode) => {
+                    --destTableSize;
+                    str += destNode;
+                    str += printWeight ? `:${weight}` : '';
+                    str += destTableSize > 0 ? ', ' : '';
+                });
+                str += ']';
+            }
+            else {
+                str += 'null';
+            }
+
+            console.log(str);
+        });
     }
 }
+
+/*
+
+
+A -> [B: 1, C: 2]
+B -> null
+C -> 
+D
+
+
+*/
